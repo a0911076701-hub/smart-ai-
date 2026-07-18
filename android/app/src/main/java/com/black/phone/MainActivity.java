3d5a93396d1c62b5a79bdc1f98bb6977ac3d53e5
a0911076701-hub <a0911076701@gmail.com>
2026-07-18 12:57:26 +0200

    🚀 المشروع الجديد - ZakyAI

---
+++ android/app/src/main/java/com/black/phone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.black.phone;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("✅ ZakyAI يعمل!");
+        setContentView(tv);
+    }
+}
